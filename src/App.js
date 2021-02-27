f9471df49320466d727aac028d7abfcf411e029a
madhupprasad-linx <madhupprasad@apiplatform.io>
2021-02-27 15:01:25 +0530

    firebase-prebuilt-css-auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { start } from "./firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    start();
+  }
+  render() {
+    return (
+      <div>
+        <div id="firebaseui"></div>
+        <div id="loader">Loading...</div>
+      </div>
+    );
+  }
 }
 
 export default App;
